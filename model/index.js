6e8d88e64af04be7a629af044309771199cf4512
David Wallace <dwallacemarketing@gmail.com>
2021-03-11 12:52:14 -0500

    updates

--- model/index.js
+++ model/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
 const Apartment = require('./apartment');
 
-User.hasMany(Apartment, { as: 'reviewer' });
+// User.hasMany(Apartment, { as: 'reviewer' });
 
 module.exports = { User, Apartment };
